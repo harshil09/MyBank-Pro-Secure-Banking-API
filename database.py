130609ef566a89c166fef52a469aeb3b9f8f5545
harshil09 <42977498+harshil09@users.noreply.github.com>
2026-04-06 14:04:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
 import os
 
+# Persistent DB location on Render
 DB_PATH = os.path.join("/opt/render/data", "user.db")
 
 def get_connection():
-    conn= sqlite3.connect(DB_PATH, check_same_thread=False)
-    return conn
+    # Ensure the folder exists
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+    return sqlite3.connect(DB_PATH, check_same_thread=False)
