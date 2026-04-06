01afd52615914e2fe0295ce1bf3075495542f278
harshil09 <42977498+harshil09@users.noreply.github.com>
2026-04-06 14:03:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
-from database import get_connection
+import sqlite3
+import os
+
+DB_PATH = os.path.join("/opt/render/data", "user.db")
+
+def get_connection():
+    return sqlite3.connect(DB_PATH, check_same_thread=False)
 
 def create_table():
     conn = get_connection()
     cursor = conn.cursor()
     
+    # Users
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,15 +19,16 @@ def create_table():
     )
     """)
 
+    # Accounts (1 account per user)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS accounts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
+        user_id INTEGER PRIMARY KEY,
         balance REAL DEFAULT 0,
         FOREIGN KEY(user_id) REFERENCES users(id)
     )
     """)
 
+    # Transactions
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS transactions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
